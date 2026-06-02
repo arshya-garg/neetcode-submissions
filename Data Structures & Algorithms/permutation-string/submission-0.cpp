3557140be17a1173b239db9008e45bfa13fcd01c
Arshya Garg <80321417+arshya-garg@users.noreply.github.com>
2026-06-02 22:20:49 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+       int n = s1.length();
+       int m = s2.length();
+       sort(s1.begin(),s1.end());
+       for(int i=0;i<m-n+1;i++){
+        string temp = s2.substr(i,n);
+        sort(temp.begin(),temp.end());
+            if(s1==temp){
+                return true;
+            }
+       } 
+       return false;
+    }
+};
