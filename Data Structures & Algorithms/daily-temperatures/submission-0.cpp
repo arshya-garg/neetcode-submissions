3f0ab1609a46297914ca19a7f9d4116609faad95
Arshya Garg <80321417+arshya-garg@users.noreply.github.com>
2026-06-04 21:53:18 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        vector<int> ans(n,0);
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(temperatures[i]<temperatures[j]){
+                    ans[i] = j-i;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
